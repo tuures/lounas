6d89251d7bb857decf8b0745c2a682cd729af150
Matti Kokkola <matti.kokkola@corsearch.com>
2024-12-18 20:51:29 +0200

    Update restaurants.js

--- src/restaurants.js
+++ src/restaurants.js
@@ -1,21 +1,9 @@
 export default [
-  ['Ateneum Bistro', 'https://www.facebook.com/ateneumbistro/'],
-  ['Pajazzo', 'http://www.royalravintolat.com/casinohelsinki/piazza-lounas'],
-  ['Factory Aleksi', 'http://www.ravintolafactory.com/aleksi/lounas/'],
+  ['Ateneum Bistro', 'https://www.ateneumbistro.fi/'],
   ['Mount Everest Vilhola', 'http://www.everestyeti.fi/vilhola/fi/index.php'],
-  ['Base Camp', 'http://www.basecampnepal.fi/helsinki/fi/index.php'],
-  ['Maya', 'http://www.maya.fi/lounaat.php'],
-  ['Frans & Amélie', 'https://www.raflaamo.fi/fi/helsinki/frans-amelie-helsinki#/#browsing'],
-  [
-    'Kitzens Radisson Blu Plaza',
-    'http://www.raflaamo.fi/helsinki/kitzens-helsinki-radisson-blu-plaza#'
-      + 'navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet',
-  ],
-  ['Il Siciliano', 'http://ilsiciliano2.fi/lounas/'],
+  ['Il Siciliano', 'https://espa.ilsiciliano.fi/lounas/'],
   ['La Torrefazione', 'http://www.latorre.fi/toimipiste/aleksanterinkatu'],
-  ['Lost in Helsinki', 'https://www.lostinhelsinki.fi/lunch'],
   ['Fratello Torrefazione', 'http://www.latorre.fi/toimipiste/fratello'],
-  ['Home Taste', 'http://hometaste.fi/buffet/'],
   ['Woolshed', 'http://www.woolshed.eu/#!lounas-menu/ceik'],
   ['Belge', 'http://www.belge.fi/belge-lounas.html'],
   ['Morrisonʼs', 'http://www.lounaat.fi/#morrisons'],
@@ -24,19 +12,33 @@ export default [
     'http://www.raflaamo.fi/fi/helsinki/memphis-hesari#'
       + 'navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet',
   ],
-  ['Leonardo', 'http://www.leonardobarristorante.fi/lounas'],
-  ['Sports Academy', 'http://www.sportsacademy.fi/lounas.html'],
   ['Zucchini', 'https://www.facebook.com/pages/Zucchini/195785583799461'],
-  ['Hard Rock Cafe', 'http://www.hardrock.com/cafes/helsinki/menu.aspx#LunchMenu'],
-  ['Lo Stretto', 'http://www.lostretto.fi/menu-lounas-ja-a-la-carte'],
   ['Harald', 'https://www.ravintolaharald.fi/helsinki/lounas/'],
-  ['Vuorikadun kanttiini (Smarteat)', 'http://www.smarteat.fi/menu-smarteat-vuorikatu/'],
-  ['Sori Taproom Vuorikatu', 'https://www.lounaat.info/lounas/sori-taproom/helsinki']
+  ['Dylan', 'https://www.dylan.fi/marmoripiha'],
+  ['Retro Enoteca', 'https://retroenoteca.fi/lunch/'],
+  ['Cafe #5 (Vietnamese)', 'https://no5coffee.fi/'],
+  ['Pompier', 'https://pompier.fi/espa/'],
+  ['Tawook Lab (Syrian)', 'https://tawook-lab.choiceqr.com/online-menu/section:lounas-menu'],
+  ['Onni Nepalese', 'https://ravintolaonninepal.fi/'],
+  ['Bastard Burgers', 'https://bastardburgers.com/fi/restaurants/mikonkatu/'],
+  ['Baba Döner', 'https://babadoner.fi/'],
+  ['99 Top Meal', 'https://social.quandoo.com/en/groups/99-topmeal-101562'],
+  ['Rioni', 'https://www.rioni.fi/helsinki'],
+  ['The Cock', 'https://www.thecock.fi/'],
+  ['Bangkok 9', 'https://www.bangkok9restaurant.com/lounaslista'],
+  ['Biang', 'https://biang.fi/restaurants/helsinki-citycenter'],
+  ['Via Tribunali', 'https://viatribunali.fi/helsinki-keskusta/'],
+  ['Sofia Helsinki', 'https://sofiahelsinki.fi/sofia-ravintola-ja-cafe-bar/lounas/'],
+  ['16 Boom', 'https://www.quandoo.fi/en/place/16-boom-92322'],
+  ['Hani Cafe', 'https://www.hanicafechi.fi/'],
+  ['Swad Indian Bistro', 'https://swad.fi/']
 ]
 
 // Temporarily out-of-service
 // ['Pompier Espa', 'http://pompier.fi/espa/lounas/'],
 // ['Tablo Ateneum', 'https://www.facebook.com/TabloAteneum'],
+//  ['Maya', 'http://www.maya.fi/lounaat.php'],
+
 
 // RIP
 // ['Café Balzac', 'http://www.cafebalzac.fi/menu.php'],
@@ -44,3 +46,24 @@ export default [
 // ['Armas', 'http://armashelsinki.fi/'],
 // ['Dong Bei Hu', 'http://www.dongbeihu.fi/'],
 // ['Shanghai Cowboy', 'https://www.facebook.com/ShanghaiCowboyHKI/'],
+//  ['Vuorikadun kanttiini (Smarteat)', 'http://www.smarteat.fi/menu-smarteat-vuorikatu/'],
+//  ['Sori Taproom Vuorikatu', 'https://www.lounaat.info/lounas/sori-taproom/helsinki']
+
+//  ['Base Camp', 'http://www.basecampnepal.fi/helsinki/fi/index.php'],
+//  ['Pajazzo', 'http://www.royalravintolat.com/casinohelsinki/piazza-lounas'],
+//  ['Factory Aleksi', 'http://www.ravintolafactory.com/aleksi/lounas'],
+//     ['Frans & Amélie', 'https://www.raflaamo.fi/fi/helsinki/frans-amelie-helsinki#/#browsing'],
+//  [
+//    'Kitzens Radisson Blu Plaza',
+//    'http://www.raflaamo.fi/helsinki/kitzens-helsinki-radisson-blu-plaza#'
+//      + 'navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet',
+//  ],
+//  ['Lost in Helsinki', 'https://www.lostinhelsinki.fi/lunch'],
+//  ['Home Taste', 'http://hometaste.fi/buffet/'],
+//  ['Hard Rock Cafe', 'http://www.hardrock.com/cafes/helsinki/menu.aspx#LunchMenu'],
+//  ['Leonardo', 'http://www.leonardobarristorante.fi/lounas'],
+//    ['Sports Academy', 'http://www.sportsacademy.fi/lounas.html'],
+//    ['Lo Stretto', 'http://www.lostretto.fi/menu-lounas-ja-a-la-carte'],
+
+
+
